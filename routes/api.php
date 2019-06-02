c53911db1abc44c680555d6b876749165b30ae1c
bakr-rajab <abobakr.r.bakry@gmail.com>
2019-06-02 10:10:48 +0200

    create our models and controllers then write my routes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,50 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+//Route::resource('buyer','Buyer\BuyerController',['only'=>['index','show']]); // just index and show
+//Route::resource('buyer','Buyer\BuyerController',['except'=>['index','show']]); // all without  index and show
+
+
+/**
+ * User
+ */
+Route::resource('user','User\UserController',['except'=>['create','edit']]);
+
+
+/**
+ * Buyer
+ */
+Route::resource('buyer','Buyer\BuyerController',['only'=>['index','show']]);
+
+
+/**
+ * Seller
+ */
+Route::resource('seller','Seller\SellerController',['only'=>['index','show']]);
+
+
+/**
+ * Product
+ */
+Route::resource('product','Product\ProductController',['only'=>['index','show']]);
+
+
+/**
+ * Transaction
+ */
+Route::resource('transaction','Transaction\TransactionController',['only'=>['index','show']]);
+
+
+/**
+ * Category
+ */
+Route::resource('category','Category\CategoryController',['only'=>['index','show']]);
+
